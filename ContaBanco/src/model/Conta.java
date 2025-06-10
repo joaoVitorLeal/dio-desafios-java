1ebe0eeaeea3a50004958abe2ffd6c775048ec69
joaoVitorLeal <joaoleal98@outlook.com>
2025-06-09 23:55:18 -0300

    Classe POJO para Conta bancária

--- ContaBanco/src/model/Conta.java
+++ ContaBanco/src/model/Conta.java
@@ -2,6 +2,7 @@ package model;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Locale;
 import java.util.Objects;
 
 public class Conta implements Serializable {
@@ -57,8 +58,11 @@ public class Conta implements Serializable {
 
     @Override
     public String toString() {
-        return String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque",
-                nomeCliente, agencia, numero, saldo);
+        return String.format(
+                Locale.US,
+                "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque",
+                nomeCliente, agencia, numero, saldo
+        );
     }
 
     @Override
